16620eb647873a2809643243001b628c157cb6bd
Megan Wu <meganwu1994@gmail.com>
2019-07-08 14:01:53 -0500

    survey form created

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var surveyArray = [
+    {
+      name: "Blaire",
+      number: "000-000-0000"
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = surveyArray;
