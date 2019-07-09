29f5ba02613ba1c6075a1a72cb595d89afb7516f
Megan Wu <meganwu1994@gmail.com>
2019-07-08 23:48:09 -0500

    added server.js, and others

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,155 @@
 var surveyArray = [
-    {
-      name: "Blaire",
-      number: "000-000-0000"
-    }
-  ];
-  
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = surveyArray;
+{
+    name: 'Blaire',
+    scores: [
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1'
+    ]
+},
+{
+    name: 'Bella',
+    scores: [
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5'
+    ]
+},
+{
+    name: 'Spencer',
+    scores: [
+        '4',
+        '2',
+        '5',
+        '1',
+        '1',
+        '3',
+        '5',
+        '1',
+        '2',
+        '2'
+    ]
+},
+{
+    name: 'Chloe',
+    scores: [
+        '4',
+        '2',
+        '2',
+        '4',
+        '3',
+        '2',
+        '3',
+        '3',
+        '3',
+        '2'
+    ]
+},
+{
+    name: 'Zac',
+    scores: [
+        '1',
+        '1',
+        '2',
+        '4',
+        '5',
+        '1',
+        '5',
+        '5',
+        '5',
+        '2'
+    ]
+},
+{
+    name: 'Ian',
+    scores: [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '4',
+        '3',
+        '2',
+        '1',
+        '3'
+    ]
+},
+{
+    name: 'Elaine',
+    scores: [
+        '1',
+        '4',
+        '1',
+        '2',
+        '1',
+        '2',
+        '5',
+        '2',
+        '4',
+        '4'
+    ]
+},
+{
+    name: 'Kelsey',
+    scores: [
+        '4',
+        '1',
+        '3',
+        '2',
+        '5',
+        '4',
+        '3',
+        '5',
+        '1',
+        '2'
+    ]
+},
+{
+    name: 'Rick',
+    scores: [
+        '5',
+        '5',
+        '1',
+        '5',
+        '2',
+        '3',
+        '5',
+        '5',
+        '4',
+        '5'
+    ]
+},
+{
+    name: 'Wesley',
+    scores: [
+        '1',
+        '5',
+        '4',
+        '2',
+        '1',
+        '1',
+        '3',
+        '4',
+        '1',
+        '3'
+    ]
+}
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = surveyArray;
